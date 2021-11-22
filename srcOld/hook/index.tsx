d216c92104200252aee3b3cf3cb34e41f978aef2
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-11-22 20:44:31 -0300

    Removing files

--- srcOld/hook/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { MapCoordinates } from './MapCoordinates';
-
-const AppProvider: React.FC = ({ children }) => {
-  return <MapCoordinates>{children}</MapCoordinates>;
-};
-
-export default AppProvider;
